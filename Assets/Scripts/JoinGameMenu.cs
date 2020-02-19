80bdfaa1f7da1471b86cea205755adafc3315242
Alexey Taran <alextar04@yandex.ru>
2020-02-19 22:37:51 +0300

    added basic scripts to windows

---
+++ Assets/Scripts/JoinGameMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JoinGameMenu : MonoBehaviour
+{
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void TryToJoin()
+    {
+        SceneManager.LoadScene(5);
+    }
+}
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void OpenCreateGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenJoinGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OpenRecords()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
